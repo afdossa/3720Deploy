846e8d76039671b2add4eb5b2fe6989b67941ab1
Andre Dos Santos <afdossa@clemson.edu>
2025-11-17 21:22:08 -0500

    WORKING!!!!!!!!!!!!

--- backend/server.js
+++ backend/server.js
@@ -277,3 +277,4 @@ app.get('/api/profile', protect, getProfile);
 app.use((req, res) => {
     res.status(404).json({ message: `Route not found: ${req.method} ${req.originalUrl}` });
 });
+//Working!!!!!
